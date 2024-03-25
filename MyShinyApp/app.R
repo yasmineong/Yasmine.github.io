b17dd8355c011e0f2cd3e885c5dd7a1b980076b9
yasmineong <yasmineong25@gmail.com>
2024-03-25 21:20:03 +0800

    testing shiny app change

--- MyShinyApp/app.R
+++ MyShinyApp/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -21,7 +12,7 @@ ui <- fluidPage(
             sliderInput("bins",
                         "Number of bins:",
                         min = 1,
-                        max = 50,
+                        max = 60,
                         value = 30)
         ),
 
@@ -41,7 +32,7 @@ server <- function(input, output) {
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+        hist(x, breaks = bins, col = 'red', border = 'white',
              xlab = 'Waiting time to next eruption (in mins)',
              main = 'Histogram of waiting times')
     })
